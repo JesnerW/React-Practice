a490d4189108c8b7ccf7f85179b249d44cfa358f
JesnerW <jesner631@gmail.com>
2025-05-18 21:00:07 -0500

    new

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Padre } from './07-tarea-memo/Padre.jsx'
-  
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Padre />
-  </StrictMode>,
-)
+
+import './08-useReducer/intro-reducer.js'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Padre />
+//   </StrictMode>,
+// )
